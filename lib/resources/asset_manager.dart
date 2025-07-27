2db445f488b67be5c3398688354e397e2d4e60f7
AabhashRai13 <raiaabhash3@gmail.com>
2025-07-27 22:59:30 +1000

    changes

--- lib/resources/asset_manager.dart
+++ lib/resources/asset_manager.dart
@@ -6,7 +6,8 @@ class ImageAssets {
   static const String iosIcon = '$imagePath/ios_icon.png';
   static const String androidIcon = '$imagePath/android_icon.png';
   static const String meditatingMan = '$imagePath/aabhash_meditating.png';
-  static const String meditatingManLowRes = '$imagePath/meditating_man_low_res.jpeg';
+  static const String meditatingManLowRes = 
+  '$imagePath/meditating_man_low_res.jpeg';
   static const String mandala = '$imagePath/mandala.png';
   static const String light = '$imagePath/light.png';
   static const String creamCurtain = '$imagePath/cream_curtain.png';
